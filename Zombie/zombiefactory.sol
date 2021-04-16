81d406fa1ffd4ac6b2022915ce62cdae422c65e1
szczeprez <szczeprez@poczta.fm>
2021-04-16 09:44:51 +0200

    refactor and add feeding contract

--- Zombie/zombiefactory.sol
+++ Zombie/zombiefactory.sol
@@ -19,10 +19,12 @@ contract ZombieFactory {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) private {
-        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
-        
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
-    } 
+    }
+
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
@@ -30,6 +32,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
---
+++ Zombie/zombiefeeding.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.3; 
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultyply(uint _zombieId, uint _targetDna) public {
+
+  }
+  
+}
